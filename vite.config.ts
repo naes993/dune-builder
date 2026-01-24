7953d4a07028c7c33d201c36f79fa138c9732ce4
naes993 <sean@mmtechdesigns.com>
2026-01-23 20:01:54 -0500

    Change base path in Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/dune-builder/',
-  server: {
+base: '/',  server: {
     port: 3000,
     host: '0.0.0.0',
   },
